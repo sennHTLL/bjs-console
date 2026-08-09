168f5dc071066b15c697d35fb6e8c29d135f028c
senthecode <senthecode@example.com>
2026-08-09 20:40:50 +0500

    [v2.1.0] · giving score n' writing average in file

--- Main.java
+++ Main.java
@@ -8,12 +8,18 @@ public class Main {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
 
-    Judge[] judge = new Judge[1];
-    judge[0] = new Judge("judge 1");
+    Judge[] judge = new Judge[3];
+    judge[0] = new Judge("judge · 1");
+    judge[1] = new Judge("judge · 2");
+    judge[2] = new Judge("judge · 3");
 
-    String filePathNominationKids = "data/1v1_kids.txt";
-    String filePathNominationJunior = "data/1v1_junior.txt";
-    String filePathNominationPro = "data/1v1_pro.txt";
+    String filePathNominationKids = "data/list-kids.txt";
+    String filePathNominationJunior = "data/list-junior.txt";
+    String filePathNominationPro = "data/list-pro.txt";
+
+    String filePathNominationKidsResults = "data/results-kids.txt";
+    String filePathNominationJuniorResults = "data/results-junior.txt";
+    String filePathNominationProResults = "data/results-pro.txt";
 
     Nomination nominationKids = new Nomination("1v1 kids");
     Nomination nominationJunior = new Nomination("1v1 junior");
@@ -39,25 +45,50 @@ public class Main {
     nominationJunior.getList();
 
     IO.println("\n ===== PRO =====");
-    IO.println("GET PARTICIPANTS FROM FILE");
-    nominationPro.getParticipantsFromFile(filePathNominationPro);
-    nominationPro.getList();
-    // nominationPro.addParticipant(scanner, filePathNominationPro);
+    manageNomination(scanner, judge, nominationPro,
+        filePathNominationPro, filePathNominationProResults, scoreNominationPro);
+    // IO.println("GET PARTICIPANTS FROM FILE");
+    // nominationPro.getParticipantsFromFile(filePathNominationPro);
     // nominationPro.getList();
+    // // nominationPro.addParticipant(scanner, filePathNominationPro);
+    // // nominationPro.getList();
+    //
+    // scoreNominationPro.score(judge, nominationPro, filePathNominationProResults);
+    //
+    // // IO.println("\n UPDATE PARTICIPANT");
+    // // IO.println("[what you want to change \n [1] · name \n [2] · age \n [3] ·
+    // both
+    // // \n [other] · nothing");
+    // // nominationPro.updateParticipant(filePathNominationPro, 1, 3);
+    // // IO.println("\n LIST PARTICIPANT");
+    // // nominationPro.getList();
+    // // IO.println("\n REMOVE PARTICIPANT");
+    // // nominationPro.removeParticipant(filePathNominationPro, 1);
+    // // IO.println("\n LIST PARTICIPANT");
+    // // nominationPro.getList();
 
-    scoreNominationPro.score(judge, nominationPro);
+    scanner.close();
+  }
+
+  public static void manageNomination(Scanner scan, Judge[] judge, Nomination nom,
+      String fp, String fpr, Scoring scoreNom) {
+    IO.println("GET PARTICIPANTS FROM FILE");
+    nom.getParticipantsFromFile(fp);
+    nom.getList();
+    // nom.addParticipant(scan, fp);
+    // nom.getList();
+
+    scoreNom.score(judge, nom, fpr);
 
     // IO.println("\n UPDATE PARTICIPANT");
     // IO.println("[what you want to change \n [1] · name \n [2] · age \n [3] · both
     // \n [other] · nothing");
-    // nominationPro.updateParticipant(filePathNominationPro, 1, 3);
+    // nom.updateParticipant(fp, 1, 3);
     // IO.println("\n LIST PARTICIPANT");
-    // nominationPro.getList();
+    // nom.getList();
     // IO.println("\n REMOVE PARTICIPANT");
-    // nominationPro.removeParticipant(filePathNominationPro, 1);
+    // nom.removeParticipant(fp, 1);
     // IO.println("\n LIST PARTICIPANT");
-    // nominationPro.getList();
-
-    scanner.close();
+    // nom.getList();
   }
 }
--- battle/Scoring.java
+++ battle/Scoring.java
@@ -1,33 +1,49 @@
 package battle;
 
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Scanner;
 
 import classes.Judge;
 import classes.Nomination;
-import classes.Participant;
 
 public class Scoring {
-  private double score;
-
-  public void score(Judge[] judges, Nomination n) {
+  public void score(Judge[] judge, Nomination n, String filePath) {
     Scanner scanner = new Scanner(System.in);
 
     double scr = 0;
-    double num = 0;
     double avg = 0;
 
-    for (int i = 0; i < judges.length; i++) {
-      for (int j = 0; j < n.getListSize(); j++) {
-        IO.print("[participant] · " + n.getParticipant(j) + ", give score · ");
-        scr = Double.valueOf(scanner.nextLine());
-        // if (scr >= 0 && scr <= 10) {
-        // num += score;
-        // break;
-        // }
+    for (int i = 0; i < n.getListSize(); i++) {
+      double num = 0;
+      for (int j = 0; j < judge.length; j++) {
+        IO.println(judge[j]);
+
+        while (true) {
+          IO.print("[participant] · " + n.getParticipant(i) + ", give score · ");
+          scr = Double.valueOf(scanner.nextLine());
+
+          if (scr >= 0 && scr <= 10) {
+            num += scr;
+            break;
+          }
+        }
+      }
+      avg = num / judge.length;
+
+      try (BufferedWriter writer = new BufferedWriter(new FileWriter(filePath,
+          true))) {
+        writer.write(n.getParticipant(i) + ", " + avg);
+        writer.newLine();
+      } catch (FileNotFoundException e) {
+        IO.println("[error] · " + e);
+      } catch (IOException e) {
+        IO.println("[error -] · " + e);
       }
     }
-    // avg = num / j.length;
 
-    this.score = scr;
+    scanner.close();
   }
 }
